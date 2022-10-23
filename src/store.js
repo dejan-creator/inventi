30c2983dfc834a28906cf930e0172118ffd86530
Dejan <rajkovicdejan@yahoo.com>
2022-10-24 00:54:41 +0200

    update store.js

--- src/store.js
+++ src/store.js
@@ -1,12 +1,8 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  configureStore,
-} from "@reduxjs/toolkit";
+import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
-import postReducer from "./reducers/postsReducer";
+import reducer from "./reducers";
 
 const middleware = [thunk];
 
@@ -14,9 +10,6 @@ if (process.env.NODE_ENV === "development") {
   middleware.push(logger);
 }
 
-const reducer = combineReducers({
-  postReducer,
-});
 const store = configureStore(reducer, applyMiddleware(...middleware));
 
 export default store;
